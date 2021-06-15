3552c3cd66b048fdf7456c208421ba82a7e43c0f
mehakkapur123 <82378184+mehakkapur123@users.noreply.github.com>
2021-06-15 20:56:12 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,22 @@
+class Particle{
+    constructor(x, y, r ) {
+        var options = {
+            'restitution':0.8
+        }
+        this.r=r;
+        this.body = Bodies.circle(x, y,r, options);
+        this.color=color(random(0,255),random(0,255),random(0,255))
+        
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        fill(this.color)
+        ellipse(0, 0, this.r, this.r);
+        pop();
+      }
+}
